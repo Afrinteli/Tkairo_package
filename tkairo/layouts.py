1a7fafc9bf518efccef012da1832b2845fd1237a
Afrinteli <102554757+Afrinteli@users.noreply.github.com>
2023-05-15 21:10:28 +0200

    Update layouts.py

--- tkairo/layouts.py
+++ tkairo/layouts.py
@@ -1,1 +1,25 @@
+def grid(master, row, column, **options):   
 
+    widget = Frame(master)       
+
+    widget.grid(row=row, column=column, **options)     
+
+    return widget
+
+def pack_start(widget, **options):   
+
+    widget.pack(**options)      
+
+    return widget
+
+def pack_end(widget, **options):
+
+    widget.pack(side="right", **options)
+
+    return widget
+
+def place(widget, x=0, y=0, **options):
+
+    widget.place(x=x, y=y, **options)
+
+    return widget
