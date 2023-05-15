8148e283e6ebf2a9d03c0fa161871d900b8902df
Afrinteli <102554757+Afrinteli@users.noreply.github.com>
2023-05-15 20:53:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,41 @@
+import setuptools
 
+with open("README.md", "r") as fh:
+
+    long_description = fh.read()
+
+setuptools.setup(
+
+    name="tkairo",  
+
+    version="0.1.0",
+
+    author="Nathan Vilane",
+
+    author_email="learnfastwithnathan@gmail.com",
+
+    description="A utility module to shorten Tkinter code",
+
+    long_description=long_description,  
+
+    long_description_content_type="text/markdown",    
+
+    url="https://github.com/yourusername/tkairo",    
+
+    packages=setuptools.find_packages(),  
+
+    install_requires=[],  
+
+    classifiers=[
+
+        "Programming Language :: Python :: 3",
+
+        "License :: OSI Approved :: MIT License",
+
+        "Operating System :: OS Independent",  
+
+    ],
+
+    python_requires='>=3.6'
+
+)
